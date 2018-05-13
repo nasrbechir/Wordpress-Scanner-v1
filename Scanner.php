40325409bc312505f40e9588bcf140901f1b6eae
nasrbechir <39245774+nasrbechir@users.noreply.github.com>
2018-05-13 21:44:47 +0200

    Create Scanner.php

---
+++ Scanner.php
@@ -0,0 +1,65 @@
+<?php
+
+
+/*
+
+             ,----------------,              ,---------,
+        ,-----------------------,          ,"        ,"|
+      ,"                      ,"|        ,"        ,"  |
+     +-----------------------+  |      ,"        ,"    |
+     |  .-----------------.  |  |     +---------+      |
+     |  |                 |  |  |     | -==----'|      |
+     |  |  localhost      |  |  |     |         |      |
+     |  |                 |  |  |/----|`---=    |      |
+     |  |  C:\>_          |  |  |   ,/|==== ooo |      ;
+     |  |                 |  |  |  // |(((( [33]|    ,"
+     |  `-----------------'  |," .;'| |((((     |  ,"
+     +-----------------------+  ;;  | |         |,"
+        /_)______________(_/  //'   | +---------+
+   ___________________________/___  `,
+  /  oooooooooooooooo  .o.  oooo /,   \,"-----------
+ / ==ooooooooooooooo==.o.  ooo= //   ,`\--{)B     ,"
+/_==__==========__==_ooo__ooo=_/'   /___________,"
+
+
+     Changing rights will just prove that you're a script kiddie.
+ ╔═══════════════════════════ ஜ۩☆۩ஜ ══════════════════════════╗
+ .·★·.·´¯`·.·★·..    Coded By Nasr Bechir    ..·★·.·´¯`·.·★·.
+ ╚═══════════════════════════ ஜ۩☆۩ஜ ══════════════════════════╝
+             https://www.facebook.com/nasr.bechir
+
+ Dont change the right of code ;)
+
+#[Detect Version]  
+#[Detect User]  
+#[Detect Theme]  
+#[Detect Plugins]  
+Gr33tz To : webplus.tn & youcan.tn
+
+*/
+
+
+
+print("
+
+ __       __  _______         __       __  __            __         ______                                                                    
+/  |  _  /  |/       \       /  \     /  |/  |          /  |       /      \                                                                   
+$$ | / \ $$ |$$$$$$$  |      $$  \   /$$ |$$/  _______  $$/       /$$$$$$  |  _______   ______   _______   _______    ______    ______        
+$$ |/$  \$$ |$$ |__$$ |      $$$  \ /$$$ |/  |/       \ /  |      $$ \__$$/  /       | /      \ /       \ /       \  /      \  /      \       
+$$ /$$$  $$ |$$    $$/       $$$$  /$$$$ |$$ |$$$$$$$  |$$ |      $$      \ /$$$$$$$/  $$$$$$  |$$$$$$$  |$$$$$$$  |/$$$$$$  |/$$$$$$  |      
+$$ $$/$$ $$ |$$$$$$$/        $$ $$ $$/$$ |$$ |$$ |  $$ |$$ |       $$$$$$  |$$ |       /    $$ |$$ |  $$ |$$ |  $$ |$$    $$ |$$ |  $$/       
+$$$$/  $$$$ |$$ |            $$ |$$$/ $$ |$$ |$$ |  $$ |$$ |      /  \__$$ |$$ \_____ /$$$$$$$ |$$ |  $$ |$$ |  $$ |$$$$$$$$/ $$ |            
+$$$/    $$$ |$$ |            $$ | $/  $$ |$$ |$$ |  $$ |$$ |      $$    $$/ $$       |$$    $$ |$$ |  $$ |$$ |  $$ |$$       |$$ |            
+$$/      $$/ $$/             $$/      $$/ $$/ $$/   $$/ $$/        $$$$$$/   $$$$$$$/  $$$$$$$/ $$/   $$/ $$/   $$/  $$$$$$$/ $$/             
+                                                                                                                                              
+                                                                                                                                                                                                                                                                                                                                                   
+      Wordpress Scanner V1 By Nasr Bechoir
+");
+
+
+
+eval(base64_decode('aWYoISRhcmd2WzFdKXsNCiAgICAgZGllKCJ1c2FnZSAkYXJndlswXSBodHRwOi8vd3d3LnNpdGUvd29yZHByZXNzLmNvbSIpOw0KICAgfQ0KOw0KICAgJHNpdGU9ICRhcmd2WzFdOw0KICAgICAgICBlY2hvICJcblxuWytdWytdU2Nhbm5pbmcgOiAkc2l0ZSIuJ1srXVsrXSc7DQokY2ggPSBjdXJsX2luaXQoKTsNCmN1cmxfc2V0b3B0KCRjaCwgQ1VSTE9QVF9VUkwsICIkc2l0ZSIpOw0KY3VybF9zZXRvcHQoJGNoLCBDVVJMT1BUX0hFQURFUiwgMSk7DQpjdXJsX3NldG9wdCgkY2gsIENVUkxPUFRfUkVUVVJOVFJBTlNGRVIsIDEpOw0KY3VybF9zZXRvcHQoJGNoLCBDVVJMT1BUX1VTRVJBR0VOVCwgIk1vemlsbGEvNC4wIChjb21wYXRpYmxlOyBNU0lFIDYuMDsgV2luZG93cyBOVCA1LjApIik7DQokZ2V0ID0gY3VybF9leGVjKCRjaCk7DQpjdXJsX2Nsb3NlKCRjaCk7DQogICAgaWYocHJlZ19tYXRjaCgiI1dvcmRQcmVzcyAoLio/KS8+IyIsICRnZXQsICR2ZXJzaW9uKSl7DQogICAgJHN0ciA9IHN0cl9yZXBsYWNlKCcvPicsICIiLCAkdmVyc2lvblswXSk7DQogICAgJHN0ciA9IHN0cl9yZXBsYWNlKCciJywgIiIsICRzdHIpOw0KICAgDQogICAgJHVzZXJzID0gQGZpbGVfZ2V0X2NvbnRlbnRzKCIkc2l0ZS8/YXV0aG9yPTEiKTsNCiAgICBwcmVnX21hdGNoKCcvPHRpdGxlPiguKj8pPFwvdGl0bGU+L3NpJywkdXNlcnMsJHVzZXIpOw0KICAgICR3cHVzZXIgPSBleHBsb2RlKCd8JywkdXNlclsxXSk7DQplY2hvICJcbiBVc2VyIDogIi4kd3B1c2VyWzBdOyB9DQoNCg0KZWNobyAiXG4gVmVyc2lvbiA6ICIuJHN0cjsgDQogDQogDQogJGNvbnRlbnQgPSBjVVJMKCRzaXRlKTsNCiANCiANCiANCiAJcHJlZ19tYXRjaF9hbGwoIi93cC1jb250ZW50XC90aGVtZXNcLyguKj8pXC9zdHlsZVwuY3NzLyIsICRjb250ZW50LCAkcmV0XzEpOw0KCQkJCXByZWdfbWF0Y2hfYWxsKCIvXDxtZXRhIG5hbWU9XCJnZW5lcmF0b3JcIiBjb250ZW50PVwiV29yZFByZXNzICguKj8pXCIgXC9cPi8iLCAkY29udGVudCwgJHJldF8zKTsNCgkJCQkNCgkJCQllY2hvICJcbiBUaGVtZSA6ICIudHJpbSgkcmV0XzFbMV1bMF0pIDsNCgkJCQkJcHJlZ19tYXRjaF9hbGwoIi93cC1jb250ZW50XC9wbHVnaW5zXC8oLio/KVwvLyIsICRjb250ZW50LCAkbWF0Y2gpOw0KCQkJCWZvcmVhY2goJG1hdGNoWzFdIGFzICRibHVnaW4pIA0KCQkJCXsNCgkJCQkJJFB1cmwgPSAkc2l0ZS4gIi93cC1jb250ZW50L3BsdWdpbnMvIiAuJGJsdWdpbjsNCgkJCQkJaWYoIWlzc2V0KCRBcnJbJGJsdWdpbl0pIGFuZCAhaXNfbnVsbCgkYmx1Z2luKSBhbmQgIWVtcHR5KCRibHVnaW4pKSANCgkJCQkJeyANCgkJCQkJZWNobyAiICBcbiAgLT5QbHVnaW4gOiIuJGJsdWdpbjsNCgkJCQkJfQ0KCQkJCQkkbkFycltdID0gJGJsdWdpbjsNCgkJCQkJJEFyclskYmx1Z2luXSA9IHRydWU7DQoJCQkJCQ0KCQkJCX0NCgkJCQkNCiBmdW5jdGlvbiBjVVJMKCR1cmwpIA0KCXsNCgkJJHpjVVJMID0gY3VybF9pbml0KCk7DQoJCWN1cmxfc2V0b3B0KCR6Y1VSTCwgQ1VSTE9QVF9IRUFERVIsIDEpOw0KCQljdXJsX3NldG9wdCgkemNVUkwsIENVUkxPUFRfVElNRU9VVCwgMTUpOw0KCQljdXJsX3NldG9wdCgkemNVUkwsIENVUkxPUFRfUkVUVVJOVFJBTlNGRVIsIDEpOw0KCQljdXJsX3NldG9wdCgkemNVUkwsIENVUkxPUFRfU1NMX1ZFUklGWVBFRVIsIDApOw0KCQljdXJsX3NldG9wdCgkemNVUkwsIENVUkxPUFRfVVJMLCAkdXJsKTsNCgkJY3VybF9zZXRvcHQoJHpjVVJMLCBDVVJMT1BUX1VTRVJBR0VOVCwgIk1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDUuMTsgcnY6Ny4wLjEpIEdlY2tvLzIwMTAwMTAxIEZpcmVmb3gvNy4wLjEiKTsNCgkJY3VybF9zZXRvcHQoJHpjVVJMLCBDVVJMT1BUX0ZPTExPV0xPQ0FUSU9OLCAxKTsNCgkJJHJlc3VsdCA9IGN1cmxfZXhlYygkemNVUkwpOw0KCQlpZigkcmVzdWx0KSANCgkJew0KCQkJcmV0dXJuICRyZXN1bHQ7DQoJCQkNCgkJfQ0KCX0NCiANCiA='));
+
+
+//http://www.valdenzanews.it 
+?>
